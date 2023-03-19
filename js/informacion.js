a5ef073dfb3003ecd8a521b57b007dcfe2939b56
MatiasGutierrezz <matiasmfg99@gmail.com>
2023-03-18 21:13:16 -0300

    Primer Subida
    primer commit

---
+++ js/informacion.js
@@ -0,0 +1,14 @@
+"use strict"
+const urlBlogs = "https://61117acdc38a0900171f1219.mockapi.io/BLOG";
+
+function load(){
+    console.log("LOAD INFO");
+    loadEvents();
+}
+
+function loadEvents(){
+    console.log('EVENTOS INFORMACION');
+}
+
+export{loadEvents};
+export{load};
